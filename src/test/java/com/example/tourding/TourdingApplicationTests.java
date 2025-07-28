c7477dfb977ce47c5d1ee886bfcf36d55c56f8f9
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-28 17:10:03 +0900

    [Init] Spring boot 초기설정

---
+++ src/test/java/com/example/tourding/TourdingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourdingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
