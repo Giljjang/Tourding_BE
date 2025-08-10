54fb68781c9538330019d1732a0566a934acfa18
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-08-10 22:08:23 +0900

    [DEBUG] 스웨거 서버 URL 환경변수 설정

--- src/main/java/com/example/tourding/config/SwaggerConfig.java
+++ src/main/java/com/example/tourding/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +14,20 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${server.port:8080}")
+    private String serverPort;
+
+    @Value("${swagger.server.url:http://localhost}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
+        String baseUrl = serverUrl + ":" + serverPort;
+        
         return new OpenAPI()
                 .info(apiInfo())
                 .servers(List.of(
+                        new Server().url(baseUrl).description("현재 서버"),
                         new Server().url("http://localhost:8080").description("로컬 개발 서버"),
                         new Server().url("http://3.38.209.142:8080").description("EC2 프로덕션 서버")
                 ));
