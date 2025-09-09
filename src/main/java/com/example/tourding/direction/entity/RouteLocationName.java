818def09c2e253efcdece26579cccef14ad35c14
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-09-10 02:49:51 +0900

    [Chore] 테이블 스키마 지정

--- src/main/java/com/example/tourding/direction/entity/RouteLocationName.java
+++ src/main/java/com/example/tourding/direction/entity/RouteLocationName.java
@@ -9,7 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
+@Table(name = "route_location", schema = "tourding")
 public class RouteLocationName {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
